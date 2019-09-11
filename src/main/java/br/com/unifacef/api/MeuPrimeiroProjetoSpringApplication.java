45768ed35f1c3b150fee030ddaf9a991befb7d95
cairoas99 <cairo.a.santos@gmail.com>
2019-09-10 22:28:53 -0300

    aula 10-09

--- src/main/java/br/com/unifacef/api/MeuPrimeiroProjetoSpringApplication.java
+++ src/main/java/br/com/unifacef/api/MeuPrimeiroProjetoSpringApplication.java
@@ -1,15 +1,22 @@
 package br.com.unifacef.api;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+
+import br.com.unifacef.api.entities.Empresa;
 
 //import br.com.unifacef.api.utils.SenhaUtils;
 
 @SpringBootApplication
 public class MeuPrimeiroProjetoSpringApplication {
 
+	@Autowired
+	private EmpresaRepository empresaRepository;
 	public static void main(String[] args) {
 		SpringApplication.run(MeuPrimeiroProjetoSpringApplication.class, args);
 		//System.out.println("Alteramos o arquivo application.properties ");
@@ -24,6 +31,21 @@ public class MeuPrimeiroProjetoSpringApplication {
 			senhaEncoded = SenhaUtils.gerarBCrypt("123456");
 			System.out.println("Senha encoded novamente:" + senhaEncoded);
 			System.out.println("Senha válida: " + SenhaUtils.senhaValida("123456", senhaEncoded));*/
+			Empresa empresa = new Empresa();
+			empresa.setRazaoSocial("Kazale IT");
+			empresa.setCnpj("74645215000104");
+			this.empresaRepository.save(empresa);
+			List<Empresa> empresas = empresaRepository.findAll();
+			empresas.forEach(System.out::println);
+			Empresa empresaDb = empresaRepository.findById(1L).orElse(null);
+			System.out.println("Empresa por ID: " + empresaDb);
+			//empresaDb.setRazaoSocial("Kazale IT Web");
+			//this.empresaRepository.save(empresaDb);
+			Empresa empresaCnpj = empresaRepository.findByCnpj("74645215000104");
+			System.out.println("Empresa por CNPJ: " + empresaCnpj);
+			//this.empresaRepository.delete(1L);
+			empresas = empresaRepository.findAll();
+			System.out.println("Empresas: " + empresas.size());
 		};
 	}
 
