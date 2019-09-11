41ca784f617390923c6c949de4cc7a47e7414825
cairoas99 <cairo.a.santos@gmail.com>
2019-09-10 21:46:29 -0300

    comeco aula 10-09

--- src/main/java/br/com/unifacef/api/EmpresaRepository.java
+++ src/main/java/br/com/unifacef/api/EmpresaRepository.java
@@ -1,5 +1,10 @@
 package br.com.unifacef.api;
 
-public interface EmpresaRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
 
+import br.com.unifacef.api.entities.Empresa;
+
+public interface EmpresaRepository extends JpaRepository<Empresa, Long> {
+
+	Empresa findByCnpj(String cnpj);
 }
--- src/main/java/br/com/unifacef/api/MeuPrimeiroProjetoSpringApplication.java
+++ src/main/java/br/com/unifacef/api/MeuPrimeiroProjetoSpringApplication.java
@@ -5,25 +5,25 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
-import br.com.unifacef.api.utils.SenhaUtils;
+//import br.com.unifacef.api.utils.SenhaUtils;
 
 @SpringBootApplication
 public class MeuPrimeiroProjetoSpringApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MeuPrimeiroProjetoSpringApplication.class, args);
-		System.out.println("Alteramos o arquivo application.properties ");
-		System.out.println("de modo que não precisemos configurar agora a fonte de dados do MySQL ");
+		//System.out.println("Alteramos o arquivo application.properties ");
+		//System.out.println("de modo que não precisemos configurar agora a fonte de dados do MySQL ");
 	}
 
 	@Bean
 	public CommandLineRunner commandLineRunner() {
 		return args -> {
-			String senhaEncoded = SenhaUtils.gerarBCrypt("123456");
+			/*String senhaEncoded = SenhaUtils.gerarBCrypt("123456");
 			System.out.println("Senha encoded: " + senhaEncoded);
 			senhaEncoded = SenhaUtils.gerarBCrypt("123456");
 			System.out.println("Senha encoded novamente:" + senhaEncoded);
-			System.out.println("Senha válida: " + SenhaUtils.senhaValida("123456", senhaEncoded));
+			System.out.println("Senha válida: " + SenhaUtils.senhaValida("123456", senhaEncoded));*/
 		};
 	}
 
